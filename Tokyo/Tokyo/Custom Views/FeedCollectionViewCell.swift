d99ecefcb4454802048686baf0ff945f45bae00e
Hugo Prinsloo <hugo.prinsloo@icloud.com>
2018-10-14 13:48:56 +0200

    Updated collectionView

---
+++ Tokyo/Tokyo/Custom Views/FeedCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCollectionViewCell.swift
+//  Tokyo
+//
+//  Created by Hugo Prinsloo on 2018/10/14.
+//  Copyright © 2018 Hugo. All rights reserved.
+//
+
+import UIKit
+
+class FeedCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Tokyo/Tokyo/SharedFile.swift
@@ -0,0 +1,9 @@
+//
+//  SharedFile.swift
+//  Tokyo
+//
+//  Created by Hugo Prinsloo on 2018/10/14.
+//  Copyright © 2018 Hugo. All rights reserved.
+//
+
+import Foundation
