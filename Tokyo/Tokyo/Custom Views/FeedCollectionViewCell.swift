a9a7d1c0578db8b7c0b72bb33b1e8b31c833c1eb
Hugo Prinsloo <hugo.prinsloo@icloud.com>
2018-10-15 22:30:06 +0200

    Configured cells

--- Tokyo/Tokyo/Custom Views/FeedCollectionViewCell.swift
+++ Tokyo/Tokyo/Custom Views/FeedCollectionViewCell.swift
@@ -11,10 +11,15 @@ import UIKit
 class FeedCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleBackgroundView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-
+        
+        titleBackgroundView.layer.cornerRadius = 16
+        titleBackgroundView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        setup()
     }
     
     func configure(for item: SharedFile) {
@@ -22,5 +27,20 @@ class FeedCollectionViewCell: UICollectionViewCell {
             imageView.image = item.image
         }
     }
-
+    
+    private func setup() {
+        backgroundColor = UIColor.lightGray
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 4)
+        layer.masksToBounds = false
+        layer.shadowRadius = 8
+        layer.shadowOpacity = 0.1
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.layer.cornerRadius = 16
+        layer.cornerRadius = 16
+    }
 }
+
